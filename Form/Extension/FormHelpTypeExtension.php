aa7cace8709dc27cf9e7b7776d167274c2fef932
Thomas Jarrand <thomas.jarrand@gmail.com>
2014-11-20 10:17:10 +0100

    Transfering Help value to view

--- Form/Extension/FormHelpTypeExtension.php
+++ Form/Extension/FormHelpTypeExtension.php
@@ -10,6 +10,8 @@
 
 namespace Elao\Bundle\FormBundle\Form\Extension;
 
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
@@ -34,5 +36,15 @@ class FormHelpTypeExtension extends AbstractTypeExtension
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array('help' => false));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        if ($options['help']) {
+            $view->vars['help'] = $options['help'];
+        }
     }
 }
